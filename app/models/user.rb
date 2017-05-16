9a858499a49ed9019a4d7ef12948714d1e034fdb
Gary Yeh <garyeh@gmail.com>
2017-05-16 11:09:35 -0700

    Begin backend auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true
@@ -11,7 +23,24 @@ class User < ApplicationRecord
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_token
+    self.save
+    self.session_token
+  end
+
   private
+
   def ensure_session_token
     self.session_token ||= User.generate_token
   end
