df4fee1cc72b7c128a9de10a3ed2b7f22dddf702
Gary Yeh <garyeh@gmail.com>
2017-05-16 16:45:19 -0700

    Add AuthRoute and Bootstrapping

--- frontend/clickstarter.jsx
+++ frontend/clickstarter.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { signup, login, logout } from './util/session_api_util';
-
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
-
-  window.store = store;
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 
-const App = () => (
+import SessionFormContainer from './session_form/session_form_container';
+import GreetingContainer from './greeting/greeting_container';
+
+const App = (props) => (
   <div>
     <header>
       <Link to="/">
         <h1>Clickstarter</h1>
       </Link>
+      <GreetingContainer />
     </header>
     <Switch>
-      <Route path="/login" />
-      <Route path="/signup" />
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
     </Switch>
   </div>
 );
