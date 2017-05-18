0cf1205f071373fa8de7fe41d4f800d151c4bd85
Gary Yeh <garyeh@gmail.com>
2017-05-18 13:27:17 -0700

    Add carousel

--- frontend/components/projects/project_index.jsx
+++ frontend/components/projects/project_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Carousel from 'nuka-carousel';
 
 class ProjectIndex extends React.Component {
   componentDidMount() {
@@ -8,10 +9,14 @@ class ProjectIndex extends React.Component {
 
   render() {
     return (
-      <div className="splash">
-        <p>{this.props.projects.map(project => project.title)}</p>
-        <img src="https://images.unsplash.com/photo-1481487196290-c152efe083f5?dpr=2&auto=compress,format&fit=crop&w=376&h=227&q=80&cs=tinysrgb&crop=&bg=" alt="Website" />
-      </div>
+      <section className="splash_content">
+        <Carousel className="splash_img">
+          <img src="https://images.unsplash.com/photo-1481487196290-c152efe083f5?dpr=2&auto=compress,format&fit=crop&w=376&h=227&q=80&cs=tinysrgb&crop=&bg=" alt="Website" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Bos_grunniens_at_Letdar_on_Annapurna_Circuit.jpg/1280px-Bos_grunniens_at_Letdar_on_Annapurna_Circuit.jpg" alt="Yakety-slack" />
+          <img src="https://images.unsplash.com/photo-1494390248081-4e521a5940db?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1079&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg=" alt="Pantry" />
+          <img src="https://res.cloudinary.com/dbxwu45pr/image/upload/v1495046731/hero-2_hiyzq5.jpg" alt="Prana" />
+        </Carousel>
+      </section>
     );
   }
 }
