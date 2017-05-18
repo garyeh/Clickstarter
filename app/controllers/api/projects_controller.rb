cb4625f3510e6d1dbdcf00b60c6014a3538576ce
Gary Yeh <garyeh@gmail.com>
2017-05-17 19:35:27 -0700

    Add project routes

---
+++ app/controllers/api/projects_controller.rb
@@ -0,0 +1,31 @@
+class Api::ProjectsController < ApplicationController
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      render :show
+    else
+      render json: @project.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :url, :description,
+      :main_image_url, :end_date, :funding_goal, :details,
+      :creator_id, :category_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
+    resources :projects, only: [:index, :show, :create, :destroy]
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
