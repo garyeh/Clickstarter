23972a0f273428a611b99ca43c6761c18484de8c
Gary Yeh <garyeh@gmail.com>
2017-05-20 18:40:10 -0700

    Add basic create labels/forms

--- frontend/components/project_form/project_form.jsx
+++ frontend/components/project_form/project_form.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 class ProjectForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div>hai</div>
+      <form className="project-create-form" >
+        <div className="create-form-content">
+          <p>Choose a category:</p>
+          <select name="expertise">
+            <option value="Select a category" disabled="true" selected="true">Select a category</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+
+          <p>Give your project a title:</p>
+          <input type="text" placeholder="Title" />
+
+          <p>Short blurb:</p>
+          <textarea name="review" className="form-description" placeholder="Description"></textarea>
+
+          <p>Upload an image:</p>
+
+          <p>End Date</p>
+          <p>Funding Goal</p>
+          <p>Details (optional)</p>
+        </div>
+      </form>
     );
   }
 }
--- frontend/components/project_form/project_form_container.js
+++ frontend/components/project_form/project_form_container.js
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
-import { createProject } from '../../actions/project_actions';
+import { createProject, receiveProjectErrors } from '../../actions/project_actions';
 import ProjectForm from './project_form';
 
-const mapStateToProps = ({ errors }) => ({
+const mapStateToProps = ({ session, errors }) => ({
+  currentUser: session.currentUser,
   errors
 });
 
 const mapDispatchToProps = dispatch => ({
-  createProject: project => dispatch(createProject(project))
+  createProject: project => dispatch(createProject(project)),
+  clearErrors: () => dispatch(receiveProjectErrors([]))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectForm);
