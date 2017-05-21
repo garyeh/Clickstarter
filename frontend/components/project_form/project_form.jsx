887747092192e6322e1a7acfb665abd5bb5b4e0b
Gary Yeh <garyeh@gmail.com>
2017-05-20 21:27:35 -0700

    Add more styling to create form

--- frontend/components/project_form/project_form.jsx
+++ frontend/components/project_form/project_form.jsx
@@ -9,35 +9,52 @@ class ProjectForm extends React.Component {
   render() {
     return (
       <form className="project-create-form" >
+        <div className="create-header">
+          <h1>Let's get started.</h1>
+          <p>Make a great first impression with your project’s title and image, and set your funding goal, campaign duration, and project category.</p>
+        </div>
         <section className="create-form-contents">
-          <p>Choose a category:</p>
-          <select name="expertise" defaultValue="0">
-            <option value="0" disabled="true">Select a category</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
+          <p><span>Choose a category</span>
+            <select name="expertise" defaultValue="0">
+              <option value="0" disabled="true">Select a category</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </p>
 
-          <p>Give your project a title:</p>
-          <input type="text" placeholder="Title" />
+          <p><span>Project Title</span>
+            <input type="text" placeholder="Title" />
+          </p>
 
-          <p>Short blurb:</p>
-          <textarea name="Description" className="form-description" placeholder="Description"></textarea>
+          <p><span>Website URL</span>
+            <input type="text" placeholder="URL" />
+          </p>
 
-          <p>Upload an image:</p>
+          <p><span>Short blurb</span>
+            <textarea name="Description" className="form-description" placeholder="Description"></textarea>
+          </p>
 
-          <p>End on date</p>
-          <input type="date" name="end_date" />
+          <p><span>Upload an image:</span>
+          </p>
 
-          <p>Funding Goal</p>
-          <input type="text" placeholder="Title" />
+          <p><span>End on date</span>
+            <input type="date" name="end_date" />
+          </p>
 
-          <p>Details (optional)</p>
-          <textarea name="Details" className="form-description" placeholder="Details"></textarea>
+          <p><span>Funding Goal</span>
+            <input type="text" placeholder="$" />
+          </p>
 
-          <input type="submit" value="Save Project!" />
+          <p><span>Details (optional)</span>
+            <textarea name="Details" className="form-description" placeholder="Details"></textarea>
+          </p>
+
+          <p><span>Save your project</span>
+            <input type="submit" value="Save Project!" />
+          </p>
         </section>
       </form>
     );
