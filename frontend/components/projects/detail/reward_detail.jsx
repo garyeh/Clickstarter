588324aa5e8eb2c9312b7f38bcd70d21dc4dffa3
Gary Yeh <garyeh@gmail.com>
2017-05-22 17:56:09 -0700

    Add reward detail component

---
+++ frontend/components/projects/detail/reward_detail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RewardDetail = ({reward, deleteReward}) => {
+  return (<section className="detailBox">
+      hai
+    </section>
+  );
+};
+
+export default RewardDetail;
--- frontend/components/projects/detail/rewards_box.jsx
+++ frontend/components/projects/detail/rewards_box.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import RewardDetail from './reward_detail';
 
 class RewardsBox extends React.Component {
+
+
   render() {
-    console.log(this.props);
+    let rewards = this.props.detail.rewards ? this.props.detail.rewards : [];
     return (<div className="rewardsBox">
         <div>
           <h2>About this project</h2>
           <p>{this.props.detail.details}</p>
         </div>
+
         <div>
           <h2>Support this project</h2>
-          <p>Hai</p>
+          <p>
+            {rewards.map(reward => (
+              <RewardDetail reward={reward} deleteReward={this.props.deleteReward} />
+            ))}
+          </p>
         </div>
       </div>
     );
