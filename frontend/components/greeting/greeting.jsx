ad2865baaabb084cb3577db3ac6611a5177fb550
Gary Yeh <garyeh@gmail.com>
2017-06-18 14:39:16 -0700

    Change avatar icon

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -15,13 +15,14 @@ const personalGreeting = (currentUser, logout, fetchSearchProjects) => (
   <hgroup className="header-group">
     <SearchBar fetchSearchProjects={fetchSearchProjects} />
     <div className="dropdown">
-      <img src="http://res.cloudinary.com/ds1qfel8a/image/upload/v1495160455/Stock/nav_dropdown_ren7yn.png"
-      alt="User icon" height="40" width="40" className="dropbtn" />
+      <img src="http://res.cloudinary.com/ds1qfel8a/image/upload/v1497820642/Stock/avatar_nqzvi4.png"
+      alt="User icon" className="dropbtn" />
+      <div className="background" />
       <div className="dropdown-content">
         <p>Your Profile</p>
         <p>You're logged in as <span>{currentUser.username}</span>
           <button className="header-button" onClick={logout}>
-            Log Out</button>
+            Log out</button>
         </p>
       </div>
     </div>
